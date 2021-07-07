bff84a9029f1947a17253c2326006c19d9c8d1cd
Macario Lou Valera Presto <40906922+Macpresto@users.noreply.github.com>
2021-07-07 22:44:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,192 +1,188 @@
-import pandas as pd
-from visual_automata.fa.dfa import VisualDFA
+
 from automata.fa.dfa import DFA
-import streamlit as st
-from PIL import Image  # IMAGE MODULE
+from visual_automata.fa.dfa import VisualDFA
+import pandas as pd 
+import streamlit as sl
 
-# TODO
-# Reset input box when clicking reset button
+#DFA regex1
+dfa1 = DFA(
+states= {'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9',
+ 'q10', 'q11', 'q12', 'q13', 'q14', 'TRAP'},
+input_symbols={'A','B'},
+transitions={
+    'q0':{'A':'q1','B':'q2'},
+    'q1':{'A':'q5','B':'q6'},
+    'q2':{'A':'q3','B':'q6'},
+    'q3':{'A':'TRAP','B':'q4'},
+    'q4':{'A':'q9','B':'q2'},
+    'q5':{'A':'TRAP','B':'q7'},
+    'q6':{'A':'q8', 'B':'TRAP'},
+    'q7':{'A':'q9', 'B':'TRAP'},
+    'q8':{'A':'TRAP','B':'q9'},
+    'q9':{'A':'q10','B':'q10'},
+    'q10':{'A':'q11','B':'q12'},
+    'q11':{'A':'q13','B':'q12'},
+    'q12':{'A':'q11','B':'q14'},
+    'q13':{'A':'q13','B':'q12'},
+    'q14':{'A':'q11','B':'q14'},
+    'TRAP':{'A':'TRAP','B':'TRAP'},
+},
+initial_state='q0',
+final_states={'q13', 'q14'}
+)
 
+#DFA regex2
+dfa2 = DFA(
+states= {'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9',
+ 'q10', 'q11', 'q12', 'q13', 'q14', 'q15', 'q16', 'q17',},
+input_symbols={'1','0'},
+transitions={
+    'q1':{'0':'q2','1':'q3'},
+    'q2':{'0':'q4','1':'q3'},
+    'q3':{'0':'q2','1':'q4'},
+    'q4':{'0':'q5','1':'q5'},
+    'q5':{'0':'q5','1':'q6'},
+    'q6':{'0':'q8', '1':'q7'},
+    'q7':{'0':'q8', '1':'q17'},
+    'q8':{'0':'q5','1':'q10'},
+    'q9':{'0':'q9','1':'q6'},
+    'q10':{'0':'q13','1':'q11'},
+    'q11':{'0':'q12','1':'q17'},
+    'q12':{'0':'q9','1':'q10'},
+    'q13':{'0':'q14','1':'q10'},
+    'q14':{'0':'q14','1':'q15'},
+    'q15':{'0':'q8','1':'q16'},
+    'q16':{'0':'q8','1':'q17'},
+    'q17':{'0':'q13','1':'q17'},
+},
+initial_state='q1',
+final_states={'q9', 'q10', 'q11', 'q12', 'q13','q14', 'q15', 'q16', 'q17'}
+)
 
-# CONSTANTS
-QUESTION = ["First Expression", "Second Expression"]
-Q1 = "( b | aa | ab ) ( a | b )´ ( bb | aba | ab )´ ( aaa | bbb ) ( a | b ) ( a | b | ab )´"
-Q2 = "( 1 | 0 )´ ( 11 | 00 | 101 | 010 ) ( 1 | 0 | 11 | 00 | 101 )´ ( 11 | 00 ) (11 | 00 | 101 )´ ( 1 | 0 ) ( 1 | 0 | 11 )´"
+###################################################################################################
+#                                     USER INTERFACE                                              #
+###################################################################################################
 
+sl.set_page_config(
+    page_icon="🦈",
+    layout="wide",
+ )
+sl.title("Automata")
 
-# Streamlit Start
-st.title("The Automata Wizard 🧙🏻‍♂️")
+#varaible declaration for regular expressions
+reg_ex1 = '(bab)*(b+a)(bab+aba)(a+b)*(aa+bb)*(b+a+bb)(a+b)*(aa+bb)'
+rex_ex2 = '(1+0)*(11+00)(00+11)*(1+0+11)(1+0+11)*(101+111)(101+111)*(1+0*+11)(1+0*+11)'
 
-st.markdown("""
-
-#### The Automata Wizard will help you check String if it is valid on 2 sample Regular Expression while simulating the path the string took in the deterministic finite automaton (DFA)
-""")
-
-image = Image.open("logo_wiz.png")
-st.sidebar.image(image)
-input_box = st.sidebar.selectbox(
-    "Expression List:", ("Select an Expression", QUESTION[0], QUESTION[1]))  # select box
+#select box for regular expression
+input_box = sl.selectbox("Select Expression:", (reg_ex1, rex_ex2)) 
 
 # Page Control
-# FIRST EXPRESSION
-if input_box == QUESTION[0]:
-    first_dfa = DFA(
-        states={'q0', 'q1', 'q2', 'q3', 'q4',
-                'q5', 'q6', 'q7', 'q8'},
-        input_symbols={'a', 'b'},
-        transitions={
-            'q0': {'a': 'q1', 'b': 'q2'},
-            'q1': {'a': 'q2', 'b': 'q2'},
-            'q2': {'a': 'q3', 'b': 'q5'},
-            'q3': {'a': 'q4', 'b': 'q5'},
-            'q4': {'a': 'q7', 'b': 'q5'},
-            'q5': {'a': 'q3', 'b': 'q6'},
-            'q6': {'a': 'q3', 'b': 'q7'},
-            'q7': {'a': 'q8', 'b': 'q8'},
-            'q8': {'a': 'q8', 'b': 'q8'}
-        },
-        initial_state='q0',
-        final_states={'q8'}
-    )
-    vis_dfa = VisualDFA(first_dfa)  # Create Visual DFA from DFA
-
-    st.header("Regular Expression:")
-    st.subheader(Q1)
-
-    # Input Form
-    with st.form("form"):
-        st.header("String Checker 🧵")
-        first_user_input = st.text_input(
-            label="Enter String To Check If Valid: ⤵")
-        submit_button = st.form_submit_button(label="Submit")
-
-    # Try catch to catch error from switching expression
-    try:
-        if not first_user_input:
-            st.write("Enter a string to simulate DFA")
-        elif submit_button:
-            if first_dfa.accepts_input(first_user_input):
-                with st.form("1st Rectangle"):
-                    st.header(
-                        "deterministic-finite-automaton (DFA) Simulation:")
-                    st.write(vis_dfa.show_diagram(first_user_input))
-                    st.subheader("String is accepted! 🧙🏻‍♂️💯🎇")
-
-                    # Table Simulation
-                    df = pd.DataFrame(
-                        vis_dfa.input_check(first_user_input))
-
-                    listy = []
-                    for x in df.iloc[:, 2]:
-                        if x == "*q8":
-                            # listy.append("Reject")
-                            listy.append("Final_State")
-                        else:
-                            listy.append("Accepted")
-
-                    df["Accepted/Final_State"] = listy
-                    st.write(df)
-                    st.header("Context-Free Grammar(CFG):")
-                    st.markdown(''' 
-                        Start symbol: S \n
-                        S → PQRTUV  \n
-                        P → b | aa | ab \n
-                        Q → aQ | bQ | ε \n
-                        R → bbR | abaR | abR | ε \n
-                        T → aaa | bbb \n
-                        U → a | b  \n		
-                        V → aε | bε | abε | ε	OR	V -> UV | abV | ε   \n
-                        ''')
-                    # Reset
-                    reset_button = st.form_submit_button(label="Reset")
-                    if reset_button:
-                        first_user_input = ("")
-            else:
-                st.header(
-                    "deterministic-finite-automaton (DFA) Simulation:")
-                st.write(vis_dfa.show_diagram(first_user_input))
-                st.subheader("String is not accepted! 🧙🏻‍♂️💢")
-    except:
-        st.write("Wrong Input!")
+# User input Box
 
 
-# SECOND EXPRESSION
-elif input_box == QUESTION[1]:
-    dfa = DFA(
-        states={'q0', 'q1', 'q2', 'q3', 'q4',
-                'q5', 'q6', 'q7', 'q8'},
-        input_symbols={'0', '1'},
-        transitions={
-            'q0': {'0': 'q1', '1': 'q2'},
-            'q1': {'0': 'q4', '1': 'q3'},
-            'q2': {'0': 'q3', '1': 'q4'},
-            'q3': {'0': 'q4', '1': 'q4'},
-            'q4': {'0': 'q5', '1': 'q6'},
-            'q5': {'0': 'q7', '1': 'q6'},
-            'q6': {'0': 'q5', '1': 'q7'},
-            'q7': {'0': 'q8', '1': 'q8'},
-            'q8': {'0': 'q8', '1': 'q8'}
-        },
-        initial_state='q0',
-        final_states={'q8'}
-    )
-    vis_dfa = VisualDFA(dfa)
+#cfg image
+dfa_slot = sl.empty()
+col3, col4 = sl.beta_columns(2)
+cfg_slot = col3.empty()
 
-    # Start
 
-    st.header("Regular Expression:")
-    st.subheader(Q2)
+if input_box == reg_ex1:
+    dfa = dfa1 
+    with cfg_slot.beta_expander("CFG", expanded = True):
+        sl.write("""
+         S -> PQRTUVXY\n
+         P -> babP|λ\n
+         Q -> b|a\n
+         R -> bab|aba\n
+         T -> aT|bT|λ\n
+         U -> aaU|bbU|λ\n
+         V -> b|a|bb\n
+         X -> aX|bX|λ\n
+         Y -> aa|bb\n
+    """)
 
-    # User input Box
-    with st.form("form"):
-        st.header("String Checker 🧵")
-        second_user_input = st.text_input(
-            label="Enter String To Check If Valid: ⤵")
-        submit_button = st.form_submit_button(label="Submit")
-
-    # Check if string is valid
-    try:
-        if not second_user_input:
-            st.write("Enter a string to simulate DFA")
-        elif submit_button:
-            if dfa.accepts_input(second_user_input):  # If accepted
-                with st.form("2nd Rectangle"):
-                    st.header(
-                        "deterministic-finite-automaton (DFA) Simulation:")
-                    st.write(vis_dfa.show_diagram(second_user_input))
-                    st.subheader("String is accepted! 🧙🏻‍♂️💯🎇")
-                    # Table SECTION
-                    df = pd.DataFrame(
-                        vis_dfa.input_check(second_user_input))
-                    listy = []
-                    for x in df.iloc[:, 2]:
-                        if x == "*q8":
-                            listy.append("Final_State")
-                        else:
-                            listy.append("Accepted")
-                    df["Accepted/Final_State"] = listy
-                    st.write(df)
-                    st.header("Context-Free Grammar(CFG):")
-                    st.markdown('''
-                        Start symbol: S \n
-                        S → PQRTUVW \n
-                        P → 1P | 0P | ε \n
-                        Q → 11 | 00 | 101 | 010 \n
-                        R → 1R | 0R | 11R | 00R | 101R | ε \n
-                        T → 11 | 00 \n
-                        U → 11U | 00U | 101U | ε \n
-                        V → 1 | 0   \n
-                        W → 1W | 0W | 11W | ε \n
-                        ''')
-                    reset_button = st.form_submit_button(label="Reset")
-                    if reset_button:
-                        second_user_input = ""
-            else:  # IF REJECTED
-                st.header(
-                    "deterministic-finite-automaton (DFA) Simulation:")
-                st.write(vis_dfa.show_diagram(second_user_input))
-                st.subheader("String is not accepted! 🧙🏻‍♂️💢")
-    except:
-        st.write("Wrong Input!")
 else:
-    st.header("⬅ Select an expression to start 🧝🏻‍♂️")
+    dfa = dfa2
+    with cfg_slot.beta_expander("CFG", expanded = True):
+        sl.write("""
+         S -> PQRTUVWYY\n
+         P -> 1P|0P|λ\n
+         Q -> 11|00\n
+         R -> 00R|11R|λ\n
+         T -> 1|011\n
+         U -> 1U|0U|11U|λ\n
+         V -> 101|111\n
+         W -> 101W|111W|λ\n
+         Y -> 1|X|11\n
+         X -> 0X|λ\n
+    """)
+
+form = col4.form(key="form")
+user_input = form.text_input(label="Enter String To Check If Valid:").upper()
+if not user_input:
+    col4.warning("Please fill out so required fields")
+
+submit = form.form_submit_button(label="Simulate")
+
+visual_dfa = VisualDFA(dfa)
+with sl.form("dfa_form"):
+    dfa_slot.write(visual_dfa.show_diagram())
+
+#sl.write(type(submit))
+if user_input:
+    try:
+        if submit:
+            if(dfa.accepts_input(user_input)):
+                col4.success('Valid!')
+            else:
+                col4.error('Invalid!')
+                
+            
+            col1, col2 = sl.beta_columns(2)
+            visual_dfa = VisualDFA(dfa)
+            str_user_input = str(user_input)
+            dfa_slot.write(visual_dfa.show_diagram(str_user_input))
+
+            dfa1_final_states =['*q13', '*q14']
+            dfa2_final_states =['*q9', '*q10', '*q11', '*q12', '*q13','*q14', '*q15',
+            '*q16', '*q17']
+            x = visual_dfa.input_check(user_input)
+
+            #check input per step and append reject or accept
+            listy = []
+            for i in x.iloc[:,2]:
+                if i == "TRAP":
+                    listy.append("Rejected")
+                else:
+                    listy.append("Accepted")
+
+            index = len(user_input) #check user input length
+            x_list = x.iloc[:,1].tolist() #create list of input
+
+            #check if finished or did not finish
+            if dfa == dfa1:
+                if str(x.iloc[index-1,2]) in dfa1_final_states:
+                    listy.append("Finished")
+                else:
+                    listy.append("DID NOT FINISH")
+                    #sl.write(str(x.iloc[index-1,2]))
+            elif dfa == dfa2:
+                if x.iloc[index-1,2] in dfa2_final_states:
+                    listy.append("Finished")
+                else:
+                    listy.append("DID NOT FINISH") 
+
+            x_list.append('END')
+            #data frame for step checking
+            col4.write('#### Tracking')
+            col4.write(pd.DataFrame({'Input': x_list,
+            'evaluation': listy,
+            }))
+
+
+            
+    except:
+        sl.write(" ")
+
+else:
+    sl.write(" ")
